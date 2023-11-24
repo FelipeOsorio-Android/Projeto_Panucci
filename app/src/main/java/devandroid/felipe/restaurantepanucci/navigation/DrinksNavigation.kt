8896b25330eb8ab7ee7c89da33ab092360b43dd6
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2023-11-23 22:06:32 -0300

    add deeplinks

--- app/src/main/java/devandroid/felipe/restaurantepanucci/navigation/DrinksNavigation.kt
+++ app/src/main/java/devandroid/felipe/restaurantepanucci/navigation/DrinksNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
@@ -12,9 +13,13 @@ import devandroid.felipe.restaurantepanucci.ui.screens.DrinksListScreen
 import devandroid.felipe.restaurantepanucci.ui.viewmodels.DrinksListViewModel
 
 internal const val drinksRoute = "drinks"
+private const val uri = "alura://panucci.com.br/drinks"
 
 fun NavGraphBuilder.drinksScreen(onNavigateToProductDetails: (ProductModel) -> Unit) {
-    composable(drinksRoute) {
+    composable(
+        drinksRoute,
+        deepLinks = listOf(NavDeepLink(uri))
+    ) {
 
         val viewModel = viewModel<DrinksListViewModel>()
         val uiState by viewModel.uiState.collectAsState()
